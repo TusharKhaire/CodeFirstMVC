eec551690092af929f0860b72eec5108619f37c2
TusharKhaire <tusharkhaire099@gmail.com>
2023-05-30 22:28:07 +0530

    Item Type -- Modify - Delete

--- TestDemoMVCCodeFirst/TestDemoMVCCodeFirst/Controllers/ItemTypeController.cs
+++ TestDemoMVCCodeFirst/TestDemoMVCCodeFirst/Controllers/ItemTypeController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 using TestDemoCodeDAL.DAL.DataConnection;
 using TestDemoCodeDAL.DAL.Entity.Masters;
 using TestDemoCodeDAL.DAL;
+using System.Net;
+using System.Globalization;
+using System.Data.Entity;
 
 namespace TestDemoMVCCodeFirst.Controllers
 {
@@ -42,12 +45,73 @@ namespace TestDemoMVCCodeFirst.Controllers
             }
         }
         [HttpGet]
-        public ActionResult Edit(long? typeId)
+        public ActionResult Edit(Guid ? id)
         {
             ViewBag.Message = null;
-            TempData["TypeId"] = typeId;
+            if(id ==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadGateway);
+            }
+            ItemType it = db.ItemType.Find(id);
+            if (it == null)
+                return HttpNotFound();
+            TempData["TypeId"] = id;
             TempData.Keep();
-            return View();
+            return View(it);
+        }
+        [HttpPost]
+        public ActionResult Edit(ItemType it)
+        {
+            ViewBag.Message = null;
+            NumberFormatInfo formatprovider = new NumberFormatInfo();
+            formatprovider.NumberDecimalSeparator = ",";
+            formatprovider.NumberGroupSeparator = ".";
+            formatprovider.NumberGroupSizes = new int[] { 2 };
+            //Double  typecode = Convert.ToDouble(TempData["TypeId"],formatprovider);
+           // var type = db.ItemType.Where(x => x.TypeId == typecode).FirstOrDefault();
+            var type = db.ItemType.Where(x => x.TypeId == it.TypeId).FirstOrDefault();
+
+            if (ModelState.IsValid)
+            {
+                if (type != null)
+                {
+                    type.TypeName = it.TypeName;
+                    type.Details = it.Details;
+                    db.Entry(type).State = EntityState.Modified;
+                    db.SaveChanges();
+                    ViewBag.Message = "Type Modify Succesfully";
+                    //ModelState.Clear();
+                    return RedirectToAction("Index");
+                }
+                else
+                    return View(it);
+            }
+            else
+            return View(it);
+        }
+
+        public ActionResult Delete(Guid ?id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var it = db.ItemType.Find(id);
+            if(it==null)
+            {
+                return HttpNotFound();
+            }
+            TempData["TypeId"] = id;
+            TempData.Keep();
+            return View(it);
+        }
+        [HttpPost]
+        public ActionResult Delete(Guid id)
+        {
+            ItemType type = db.ItemType.Find(id);
+            db.ItemType.Remove(type);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
